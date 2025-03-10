4e534ce9b7d27a75465d08819264e76a68ff6c91
jaymin dave <jaymin4724@gmail.com>
2025-03-10 20:15:12 +0530

    index.js updated

--- server/index.js
+++ server/index.js
@@ -6,6 +6,14 @@ import registrationRoute from "./routes/Registration.js";
 import affirmationsRoute from "./routes/Affirmations.js";
 import meditationTypesRoute from "./routes/MeditationTypes.js";
 import meditationActivityRoute from "./routes/MeditationActivity.js";
+import challengesRoute from "./routes/Challenges.js";
+import challengesActivityRoute from "./routes/ChallengesActivity.js";
+import challengesRequestRoute from "./routes/ChallengesRequest.js";
+import membershipRoute from "./routes/Membership.js";
+import moodDataRoute from "./routes/MoodData.js";
+import userRoute from "./routes/User.js";
+import emailRoute from "./routes/Email.js";
+import membershipPaymentRoute from "./routes/Payment.js";
 
 const app = express();
 const PORT = process.env.PORT || 7000;
@@ -30,5 +38,13 @@ app.use("/api", registrationRoute);
 app.use("/api/affirmations", affirmationsRoute);
 app.use("/api/meditation-types", meditationTypesRoute);
 app.use("/api/meditationActivity", meditationActivityRoute);
+app.use("/api/challenges", challengesRoute);
+app.use("/api/membership", membershipRoute);
+app.use("/api/mood", moodDataRoute);
+app.use("/api/", userRoute);
+app.use("/api/email", emailRoute);
+app.use("/api/challengesActivity", challengesActivityRoute);
+app.use("/api/challengesRequest", challengesRequestRoute);
+app.use("/api/membershipPayment", membershipPaymentRoute);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
